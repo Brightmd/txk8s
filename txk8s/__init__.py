7b5a0c0a5e861e3ce92ceff5ef58ef0c6d56e08f
Rachel Kelly <rkellyalso@gmail.com>
2017-12-26 12:59:11 -0800

    001: packaging fixes

--- txk8s/__init__.py
+++ txk8s/__init__.py
@@ -2,16 +2,28 @@
 Twisted implementation for Kubernetes
 """
 
-from txk8s import (_version, 
-                   TxKubernetesClient, 
-                   createPVC, 
-                   createStorageClass, 
-                   createDeploymentFromFile,
-                   createConfigMap,
-                   createService,
-                   createServiceAccount,
-                   createClusterRole,
-                   createClusterRoleBind,
-                   createIngress,
-                   createEnvVar,)
+from txk8s import _version
+from txk8s.lib import (TxKubernetesClient,
+                  createPVC,
+                  createStorageClass,
+                  createDeploymentFromFile,
+                  createConfigMap,
+                  createService,
+                  createServiceAccount,
+                  createClusterRole,
+                  createClusterRoleBind,
+                  createIngress,
+                  createEnvVar,)
 
+(_version,
+ TxKubernetesClient,
+ createPVC,
+ createStorageClass,
+ createDeploymentFromFile,
+ createConfigMap,
+ createService,
+ createServiceAccount,
+ createClusterRole,
+ createClusterRoleBind,
+ createIngress,
+ createEnvVar,) # for pyflakes
