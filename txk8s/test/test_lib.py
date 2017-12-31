83783d19bc2d832aac828eed7e928e222cbc193e
Rachel Kelly <rkellyalso@gmail.com>
2017-12-31 12:24:16 -0800

    fixing tests, ensuring travis runs - more tests to fix

--- txk8s/test/test_lib.py
+++ txk8s/test/test_lib.py
@@ -104,7 +104,7 @@ def test_createPVC(kubeConfig):
     pPVC = patch.object(client, 'V1PersistentVolumeClaim')
     pApiMethod = patch.object(client,'CoreV1Api')
     with pPVC as mPVC, pApiMethod, pCall as mCall:
-        yield lib.createPVC('a', meta, spec, namespace)
+        yield lib.createPVC(meta, spec, namespace)
         mPVC.assert_called_once_with(api_version='v1', kind='PersistentVolumeClaim', metadata=meta, spec=spec)
         mCall.assert_called_once()
 
@@ -120,7 +120,7 @@ def test_createStorageClass(kubeConfig):
     pStorage = patch.object(client, 'V1beta1StorageClass')
     pApiMethod = patch.object(client,'StorageV1beta1Api')
     with pStorage as mStorage, pApiMethod, pCall as mCall:
-        yield lib.createStorageClass('a', meta, provisioner)
+        yield lib.createStorageClass(meta, provisioner)
         mStorage.assert_called_once_with(api_version='storage.k8s.io/v1beta1', kind='StorageClass', metadata=meta, provisioner=provisioner)
         mCall.assert_called_once()
 
@@ -140,9 +140,9 @@ def test_createDeploymentFromFile(kubeConfig):
         autospec=True,
     )
     with pApiMethod as mApiMethod, pCall as mCall, pOpen:
-        yield lib.createDeploymentFromFile('a', '/path')
+        yield lib.createDeploymentFromFile('/path')
         mApiMethod.assert_called_once()
-        mCall.assert_called_once_with('a', body='data', namespace='default')
+        mCall.assert_called_once_with(namespace='default')
 
 
 @pytest.inlineCallbacks
@@ -164,7 +164,7 @@ def test_createConfigMap(kubeConfig):
     )
     with pApiMethod, pPVC, pCall as mCall:
         yield lib.createConfigMap(meta, data, namespace)
-        mCall.assert_called_once_with('a', 'grn-se-com', 'thing')
+        mCall.assert_called_once_with('grn-se-com', 'thing')
 
 
 @pytest.inlineCallbacks
@@ -186,7 +186,7 @@ def test_createService(kubeConfig):
     with pApiMethod as mApiMethod, pCall as mCall, pOpen:
         yield lib.createService('/path', namespace)
         mApiMethod.assert_called_once()
-        mCall.assert_called_once_with('a', namespace, fileData)
+        mCall.assert_called_once_with(namespace, fileData)
 
 
 @pytest.inlineCallbacks
@@ -206,9 +206,9 @@ def test_createServiceAccount(kubeConfig):
         autospec=True,
     )
     with pApiMethod as mApiMethod, pCall as mCall, pOpen:
-        yield lib.createServiceAccount('a', '/path', namespace)
+        yield lib.createServiceAccount('/path', namespace)
         mApiMethod.assert_called_once()
-        mCall.assert_called_once_with('a', namespace, fileData)
+        mCall.assert_called_once_with(namespace, fileData)
 
 
 @pytest.inlineCallbacks
@@ -227,9 +227,9 @@ def test_createClusterRole(kubeConfig):
         autospec=True,
     )
     with pApiMethod as mApiMethod, pCall as mCall, pOpen:
-        yield lib.createClusterRole('a', '/path')
+        yield lib.createClusterRole('/path')
         mApiMethod.assert_called_once()
-        mCall.assert_called_once_with('a', fileData)
+        mCall.assert_called_once_with(fileData)
 
 
 @pytest.inlineCallbacks
@@ -248,9 +248,9 @@ def test_createClusterRoleBind(kubeConfig):
         autospec=True,
     )
     with pApiMethod as mApiMethod, pCall as mCall, pOpen:
-        yield lib.createClusterRoleBind('a', '/path')
+        yield lib.createClusterRoleBind('/path')
         mApiMethod.assert_called_once()
-        mCall.assert_called_once_with('a', fileData)
+        mCall.assert_called_once_with(fileData)
 
 
 @pytest.inlineCallbacks
@@ -270,9 +270,9 @@ def test_createIngress(kubeConfig):
         autospec=True,
     )
     with pApiMethod as mApiMethod, pCall as mCall, pOpen:
-        yield lib.createIngress('a', '/path', namespace)
+        yield lib.createIngress('/path', namespace)
         mApiMethod.assert_called_once()
-        mCall.assert_called_once_with('a', namespace, fileData)
+        mCall.assert_called_once_with(namespace, fileData)
 
 
 def test_createEnvVar(kubeConfig):
